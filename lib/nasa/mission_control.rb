9db2477903f8144afe2622d1513f400d353be817
Daniel Grieve <git@cazrin.net>
2016-04-04 13:06:35 +0100

    Refactor to remove iterations over mission commands
    Didn't like the fact that I was going over the list of mission commands
    twice. I didn't see a need to store the rovers and their commands in a
    hash, so changed it to just go through the commands once over.

--- lib/nasa/mission_control.rb
+++ lib/nasa/mission_control.rb
@@ -1,25 +1,28 @@
 class NASA::MissionControl
-  attr_accessor :plateau, :rovers
+  # I'm storing the plateau width/height but the assignment didn't mention what
+  # to do with the values. I'd assume to make sure that the rovers don't move
+  # over the edge, but the test case provided doesn't cover that scenario.
+
+  attr_accessor :plateau
 
   def initialize(mission)
     mission_commands = mission.split("\n")
 
     @plateau = parse_plateau(mission_commands.shift)
-    @rovers = mission_commands.each_slice(2).map do |rover_input|
-      {
-        rover: NASA::Rover.new(rover_input.first),
-        command: rover_input.last
-      }
-    end
+    @mission_commands = mission_commands
   end
 
   def run_mission
-    mission_results = @rovers.map do |rover|
-      rover[:command].split(//).each do |input|
-        rover[:rover].command(input)
+    mission_results = @mission_commands.each_slice(2).map do |mission_command|
+      rover_location = mission_command.first
+      rover = NASA::Rover.new(rover_location)
+
+      rover_input = mission_command.last.split(//)
+      rover_input.each do |input|
+        rover.command(input)
       end
 
-      rover[:rover].report_location
+      rover.report_location
     end
 
     mission_results.join("\n")
