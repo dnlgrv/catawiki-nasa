fcb3ad9b3e643d55c49f5c044f7ffc442edd99ca
Daniel Grieve <git@cazrin.net>
2016-04-04 12:37:06 +0100

    Handle the rotate left command

--- lib/nasa/rover.rb
+++ lib/nasa/rover.rb
@@ -8,11 +8,33 @@ class NASA::Rover
 
   attr_accessor :heading, :position
 
-  def initialize(command)
-    x, y, heading = command.split
+  def initialize(input)
+    x, y, heading = input.split
 
     @position = Position.new(x.to_i, y.to_i)
     @heading = HEADINGS[heading]
+  end
+
+  def command(input)
+    case input
+    when "L"
+      rotate_left_command
+    end
+  end
+
+  private
+
+  def rotate_left_command
+    @heading = case @heading
+               when :north
+                 :west
+               when :south
+                 :east
+               when :east
+                 :north
+               when :west
+                 :south
+               end
   end
 
   class Position
--- spec/lib/nasa/rover_spec.rb
+++ spec/lib/nasa/rover_spec.rb
@@ -19,4 +19,54 @@ RSpec.describe NASA::Rover do
       expect(described_class.new("0 0 W").heading).to eq(:west)
     end
   end
+
+  describe "#command" do
+    context "when L" do
+      let(:command) { "L" }
+
+      context "when the Rover is facing North" do
+        before do
+          subject.heading = :north
+          subject.command(command)
+        end
+
+        it "rotates the Rover to face West" do
+          expect(subject.heading).to eq(:west)
+        end
+      end
+
+      context "when the Rover is facing South" do
+        before do
+          subject.heading = :south
+          subject.command(command)
+        end
+
+        it "rotates the Rover to face East" do
+          expect(subject.heading).to eq(:east)
+        end
+      end
+
+      context "when the Rover is facing East" do
+        before do
+          subject.heading = :east
+          subject.command(command)
+        end
+
+        it "rotates the Rover to face North" do
+          expect(subject.heading).to eq(:north)
+        end
+      end
+
+      context "when the Rover is facing West" do
+        before do
+          subject.heading = :west
+          subject.command(command)
+        end
+
+        it "rotates the Rover to face South" do
+          expect(subject.heading).to eq(:south)
+        end
+      end
+    end
+  end
 end
